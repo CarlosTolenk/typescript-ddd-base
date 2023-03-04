44a74a0a88c74c83dd378b15cc25dcbdedea3a90
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-03-04 07:58:30 -0400

    Refactoring OrderId to UUID

--- src/Modules/Orders/application/OrderCreator.ts
+++ src/Modules/Orders/application/OrderCreator.ts
@@ -1,17 +1,18 @@
 import { Order } from '../domain/Order';
 import { OrderRepository } from '../domain/OrderRepository';
+import { OrderId } from '../domain/value-object/OrderId';
 import { CreateOrderRequest } from './CreateOrderRequest';
 
 export class OrderCreator {
-  private readonly repository: OrderRepository;
+	private readonly repository: OrderRepository;
 
-  constructor(repository: OrderRepository) {
-    this.repository = repository;
-  }
+	constructor(repository: OrderRepository) {
+		this.repository = repository;
+	}
 
-  async run(request: CreateOrderRequest): Promise<void> {
-    const course = new Order({ id: request.id, amount: request.amount });
+	async run(request: CreateOrderRequest): Promise<void> {
+		const course = new Order({ id: new OrderId(request.id), amount: request.amount });
 
-    return this.repository.save(course);
-  }
+		return this.repository.save(course);
+	}
 }
--- src/app/backend/controllers/order/OrderPutController.ts
+++ src/app/backend/controllers/order/OrderPutController.ts
@@ -1,29 +1,30 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-import { Controller } from '../Controller';
 import { OrderCreator } from '../../../../Modules/Orders/application/OrderCreator';
+import { Controller } from '../Controller';
 
 type OrderPutRequest = Request & {
-  body: {
-    id: string;
-    amount: number;
-  };
+	body: {
+		id: string;
+		amount: number;
+	};
 };
 
 export class OrderPutController implements Controller {
-  private readonly orderCreator: OrderCreator;
+	private readonly orderCreator: OrderCreator;
 
-  constructor(orderCreator: OrderCreator) {
-    this.orderCreator = orderCreator;
-  }
-  async run(req: OrderPutRequest, res: Response): Promise<void> {
-    try {
-      const { id, amount } = req.body;
-      await this.orderCreator.run({ id, amount });
-      res.status(httpStatus.CREATED).send();
-    } catch (error) {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).send();
-    }
-  }
+	constructor(orderCreator: OrderCreator) {
+		this.orderCreator = orderCreator;
+	}
+
+	async run(req: OrderPutRequest, res: Response): Promise<void> {
+		try {
+			const { id, amount } = req.body;
+			await this.orderCreator.run({ id, amount });
+			res.status(httpStatus.CREATED).send();
+		} catch (error) {
+			res.status(httpStatus.INTERNAL_SERVER_ERROR).send();
+		}
+	}
 }
