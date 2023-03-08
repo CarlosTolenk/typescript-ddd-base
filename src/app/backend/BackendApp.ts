10d8c8da746e222ac87c431bcd07080bbb4efd93
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-03-08 16:08:42 -0400

    DomainEvent, DomainEventSubscriber Adn EventBus

--- src/app/backend/BackendApp.ts
+++ src/app/backend/BackendApp.ts
@@ -1,20 +1,48 @@
+import { Definition } from 'node-dependency-injection';
+
+// Domain
+import { EventBus } from '../../Modules/Shared/domain/EventBus';
+import { DomainEventSubscriber } from '../../Modules/Shared/domain/DomainEventSubscriber';
+import { DomainEvent } from '../../Modules/Shared/domain/DomainEvent';
+
 import { Server } from './server';
+import container from './dependency-injection';
 
 export class BackendApp {
-	server?: Server;
+  server?: Server;
 
-	async start(): Promise<void> {
-		const port = process.env.PORT ?? '5500';
-		this.server = new Server(port);
+  async start(): Promise<void> {
+    const port = process.env.PORT ?? '5500';
+    this.server = new Server(port);
 
-		return this.server.listen();
-	}
+    await this.configureEventBus();
 
-	get httpServer(): Server['httpServer'] | undefined {
-		return this.server?.getHTTPServer();
-	}
+    return this.server.listen();
+  }
 
-	async stop(): Promise<void> {
-		return this.server?.stop();
-	}
+  get httpServer(): Server['httpServer'] | undefined {
+    return this.server?.getHTTPServer();
+  }
+
+  private async configureEventBus() {
+    const eventBus = container.get<EventBus>('Shared.domain.EventBus');
+
+    eventBus.addSubscribers(this.findSubscribers());
+  }
+
+  private findSubscribers(): Array<DomainEventSubscriber<DomainEvent>> {
+    const subscriberDefinitions = container.findTaggedServiceIds('domainEventSubscriber') as Map<String, Definition>;
+    const subscribers: Array<DomainEventSubscriber<DomainEvent>> = [];
+
+    subscriberDefinitions.forEach((value: Definition, key: String) => {
+      const domainEventSubscriber = container.get<DomainEventSubscriber<DomainEvent>>(key.toString());
+      subscribers.push(domainEventSubscriber);
+    });
+
+    return subscribers;
+  }
+
+  async stop(): Promise<void> {
+    return this.server?.stop();
+  }
 }
