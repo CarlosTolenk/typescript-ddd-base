f2f76b231d772c584d67aa7b0925a4e5bcfc0c03
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-03-15 19:39:00 -0400

    Publish event to MongoDB on RabbitMQ publish error

--- tests/Modules/Shared/infrastructure/eventBus/RabbitMQEventBus.test.ts
+++ tests/Modules/Shared/infrastructure/eventBus/RabbitMQEventBus.test.ts
@@ -1,36 +1,87 @@
-import { RabbitMqConnection } from '../../../../../src/Modules/Shared/infrastructure/eventBus/rabbitMQ/RabbitMqConnection';
+import { MongoEnvironmentArranger } from '../persistence/mongo/MongoEnvironmentArranger';
+import { RabbitMQMongoClientMother } from './__mother__/RabbitMQMongoClientMother';
+import { RabbitMQConnectionMother } from './__mother__/RabbitMQConnectionMother';
+import { DomainEventFailoverPublisherMother } from './__mother__/DomainEventFailoverPublisherMother';
 import { RabbitMQEventBus } from '../../../../../src/Modules/Shared/infrastructure/eventBus/rabbitMQ/RabbitMQEventBus';
+import { RabbitMqConnection } from '../../../../../src/Modules/Shared/infrastructure/eventBus/rabbitMQ/RabbitMqConnection';
+import { DomainEventSubscriberDummy } from '../../__mocks__/DomainEventSubscriberDummy';
+import { RabbitMQConfigurer } from '../../../../../src/Modules/Shared/infrastructure/eventBus/rabbitMQ/RabbitMQConfigurer';
+import { DomainEventFailoverPublisher } from '../../../../../src/Modules/Shared/infrastructure/eventBus/DomainEventFailoverPublisher';
+import { RabbitMQqueueFormatter } from '../../../../../src/Modules/Shared/infrastructure/eventBus/rabbitMQ/RabbitMQqueueFormatter';
+import { DomainEventDummyMother } from '../../__mocks__/DomainEventDummy';
 import { OrderCreatedDomainEventMother } from '../../../Orders/domain/OrderCreatedDomainEventMother';
+import { UuidMother } from '../../domain/UuidMother';
 
 describe('RabbitMQEventBus test', () => {
-  const config = {
-    connectionSettings: {
-      username: 'guest',
-      password: 'guest',
-      vhost: '/',
-      connection: {
-        secure: false,
-        hostname: 'localhost',
-        port: 5672
-      }
-    },
-    exchangeSettings: { name: '' }
-  };
-
-  let connection: RabbitMqConnection;
+  const exchange = 'test_domain_events';
+  let arranger: MongoEnvironmentArranger;
 
   beforeAll(async () => {
-    connection = new RabbitMqConnection(config);
-    await connection.connect();
+    arranger = new MongoEnvironmentArranger(RabbitMQMongoClientMother.create());
+  });
+
+  beforeEach(async () => {
+    await arranger.arrange();
   });
 
   afterAll(async () => {
-    await connection.close();
+    await arranger.close();
   });
 
-  it('should publish events to RabbitMQ', async () => {
-    const eventBus = new RabbitMQEventBus({ connection });
+  describe('unit', () => {
+    it('should use the failover publisher if publish to RabbitMQ fails', async () => {
+      const connection = RabbitMQConnectionMother.failOnPublish();
+      const failoverPublisher = DomainEventFailoverPublisherMother.failOverDouble();
+      const eventBus = new RabbitMQEventBus({ failoverPublisher, connection, exchange });
+      const event = OrderCreatedDomainEventMother.create({
+        aggregateId: UuidMother.random(),
+        eventId: UuidMother.random(),
+        description: '',
+        amount: 450,
+        occurredOn: new Date()
+      });
 
-    await eventBus.publish([OrderCreatedDomainEventMother.random()]);
+      await eventBus.publish([event]);
+
+      failoverPublisher.assertEventHasBeenPublished(event);
+    });
+  });
+
+  describe('integration', () => {
+    let connection: RabbitMqConnection;
+    let dummySubscriber: DomainEventSubscriberDummy;
+    let configurer: RabbitMQConfigurer;
+    let failoverPublisher: DomainEventFailoverPublisher;
+    const formatter = new RabbitMQqueueFormatter('app');
+
+    beforeAll(async () => {
+      connection = await RabbitMQConnectionMother.create();
+      failoverPublisher = DomainEventFailoverPublisherMother.create();
+
+      configurer = new RabbitMQConfigurer(connection, formatter);
+    });
+
+    beforeEach(async () => {
+      await arranger.arrange();
+      dummySubscriber = new DomainEventSubscriberDummy();
+    });
+
+    afterAll(async () => {
+      await cleanEnvironment();
+      await connection.close();
+    });
+
+    it('should publish events to RabbitMQ', async () => {
+      const eventBus = new RabbitMQEventBus({ failoverPublisher, connection, exchange });
+      const event = DomainEventDummyMother.random();
+
+      await configurer.configure({ exchange, subscribers: [dummySubscriber] });
+
+      await eventBus.publish([event]);
+    });
+
+    async function cleanEnvironment() {
+      await connection.deleteQueue(formatter.format(dummySubscriber.constructor.name));
+    }
   });
 });
