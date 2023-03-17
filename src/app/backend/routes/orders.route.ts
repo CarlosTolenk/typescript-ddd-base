84a0aedea2a9893a3ce85e8a64a83f8db0a860a5
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-03-17 18:13:11 -0400

    Set up RabbitMQ as default EventBus in the DI

--- src/app/backend/routes/orders.route.ts
+++ src/app/backend/routes/orders.route.ts
@@ -5,5 +5,5 @@ import container from '../dependency-injection';
 
 export const register = (router: Router): void => {
   const orderPostController = container.get<OrderPostController>('App.controllers.order.OrderPostController');
-  router.put('/order', (req: Request, res: Response) => orderPostController.run(req, res));
+  router.post('/order', (req: Request, res: Response) => orderPostController.run(req, res));
 };
